34d3574ae5b3caa70b62310197539343ccf09065
Ivan <rylaiinsane@gmail.com>
2018-05-12 18:34:05 +0300

    Сделал покрасивее

--- task_04_01.py
+++ task_04_01.py
@@ -1,16 +1,14 @@
-with open('data.txt') as f:
-    ish_dat = f.readline().split()
-
 n = int(input())
 p = int(input())
+
+with open('data.txt') as f:
+    ish_dat = f.readline().split()
 
 with open('out-1.txt', 'w') as f:
     for number in ish_dat:
         if int(number) % n == 0:
-            del_bez_ost = str(number) + ' '
-            f.write(del_bez_ost)
+            f.write(str(number) + ' ')
 
 with open('out-2.txt', 'w') as f:
     for number in ish_dat:
-        vozv_v_step = str(int(number) ** p) + ' '
-        f.write(vozv_v_step)
+        f.write(str(int(number) ** p) + ' ')
